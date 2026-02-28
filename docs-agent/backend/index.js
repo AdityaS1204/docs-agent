f5dcb6ad0952dbbfd552888e46426ff8c6e3225a
Aditya <adityas.singh.tech@gmail.com>
2026-02-28 10:18:27 +0530

    chore: register edit, sync and section routes in express app

--- docs-agent/backend/index.js
+++ docs-agent/backend/index.js
@@ -6,6 +6,7 @@ const { DEFAULT_PORT } = require('./config/constants');
 const generateRoute = require('./routes/generate');
 const editRoute = require('./routes/edit');
 const syncRoute = require('./routes/sync');
+const sectionRoute = require('./routes/section');
 
 const app = express();
 const port = process.env.PORT || DEFAULT_PORT;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/generate', generateRoute);
 app.use('/edit', editRoute);
 app.use('/sync', syncRoute);
+app.use('/section', sectionRoute);
 
 app.get('/', (req, res) => {
     res.send('Docs Agent API is running.');
